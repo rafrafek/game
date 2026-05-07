0ddbcfbd7594e48e7cba960a77ecd30bc05c2538
rafrafek <23004737+rafrafek@users.noreply.github.com>
2026-05-07 20:38:08 +0200

    Slider

--- game2/index.service.worker.js
+++ game2/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778171354|2001865966';
+const CACHE_VERSION = '1778177366|1016627109';
 /** @type {string} */
 const CACHE_PREFIX = 'Kula Bula-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
